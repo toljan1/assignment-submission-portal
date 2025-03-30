acac69e600855f324b0e945f4b4c6c60e587cdec
Tolik Petras <toliketras4@gmail.com>
2025-03-31 00:51:02 +0300

    Added SSR to thank-you page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,34 +49,49 @@ export default function Home() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <form onSubmit={handleSubmit(onSubmit)} className="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
-        <h1 className="text-xl font-bold mb-4 text-slate-900">Assignment Submission</h1>
-        <input {...register('name')} placeholder="Name" className="w-full p-2 border rounded mb-2 text-slate-900" />
+        <h1 className="text-xl font-bold mb-4 text-slate-900 text-center">Assignment Submission</h1>
+        <div className='flex items-center justify-between'>
+          <p className='text-black'>Name*</p>
+          <input {...register('name')} placeholder="" className="max-w-2/3 p-1 border-b text-slate-900" />
+        </div>
         <p className="text-red-500 text-sm">{errors.name?.message}</p>
         
-        <input {...register('email')} placeholder="Email" className="w-full p-2 border rounded mb-2 text-slate-900" />
+        <div className='flex items-center justify-between mt-3'>
+          <p className='text-black'>Email*</p>
+          <input {...register('email')} placeholder="" className="max-w-2/3 p-1 border-b text-slate-900" />
+        </div>
         <p className="text-red-500 text-sm">{errors.email?.message}</p>
         
-        <textarea {...register('assignment_description')} placeholder="Assignment Description" className="w-full p-2 border rounded mb-2 text-slate-900" />
+        <div className='flex flex-col justify-between gap-[6px] mt-3'>
+          <p className='text-black'>Assignment Description*</p>
+          <textarea {...register('assignment_description')} placeholder="" className="w-2/3 p-1 border rounded text-slate-900 ml-auto" />
+        </div>
         <p className="text-red-500 text-sm">{errors.assignment_description?.message}</p>
         
-        <input {...register('github_repo_url')} placeholder="GitHub Repository URL" className="w-full p-2 border rounded mb-2 text-slate-900" />
+        <div className='flex items-center justify-between mt-3'>
+          <p className='text-black'>GitHub Repository URL*</p>
+          <input {...register('github_repo_url')} placeholder="" className="max-w-2/3 p-1 border-b text-slate-900" />
+        </div>
         <p className="text-red-500 text-sm">{errors.github_repo_url?.message}</p>
-        
-        <select {...register('candidate_level')} className="w-full p-2 border rounded mb-2 text-slate-700">
-          <option value="">Select Candidate Level</option>
-          {loading 
-            ? <option disabled>Loading...</option> 
-            : levels.length > 0 && levels.map(level => (
-                <option key={level} value={level}>{level}</option>
-              ))
-          }
-          {
-            levels.length < 0 && <option disabled>Error loading levels</option>
-          }
-        </select>
+
+        <div className='flex items-center justify-between mt-3'>
+          <p className='text-black'>Candidate Level*</p>
+          <select {...register('candidate_level')} className="max-w-2/3 p-1 border-b text-slate-700">
+            <option value="">Select a level</option>
+            {loading 
+              ? <option disabled>Loading...</option> 
+              : levels.length > 0 && levels.map(level => (
+                  <option key={level} value={level}>{level}</option>
+                ))
+            }
+            {
+              levels.length < 0 && <option disabled>Error loading levels</option>
+            }
+          </select>
+        </div>
         <p className="text-red-500 text-sm">{errors.candidate_level?.message}</p>
         
-        <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded">Submit</button>
+        <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded cursor-pointer mt-4">Submit</button>
       </form>
     </div>
   );
--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,30 +1,35 @@
-"use client";
-
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
 
-export default function ThankYou() {
-  const params = useSearchParams();
-  const [submittedData, setSubmittedData] = useState({ name: "", email: "", level: "" });
+interface PageProps {
+  searchParams?: Promise<{
+    name?: string;
+    email?: string;
+    level?: string;
+  }>;
+}
 
-  useEffect(() => {
-    setSubmittedData({
-      name: params.get("name") || "",
-      email: params.get("email") || "",
-      level: params.get("level") || "",
-    });
-  }, [params]);
-  
+export default async function ThankYouPage({
+  searchParams,
+}: PageProps) {
+  const data = await searchParams;
+
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100 text-center">
       <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
         <h1 className="text-xl font-bold mb-4 text-slate-900">Thank You!</h1>
-        <p className='text-slate-900'>Your assignment has been submitted successfully.</p>
-        <p className='text-slate-900'><strong>Name:</strong> {submittedData.name}</p>
-        <p className='text-slate-900'><strong>Email:</strong> {submittedData.email}</p>
-        <p className='text-slate-900 mb-4'><strong>Candidate Level:</strong> {submittedData.level}</p>
-        <Link href={'/'} className="bg-blue-500 text-white p-2 rounded">Go Back</Link>
+        <p className="text-slate-900">Your assignment has been submitted successfully.</p>
+        <p className="text-slate-900">
+          <strong>Name:</strong> {data?.name}
+        </p>
+        <p className="text-slate-900">
+          <strong>Email:</strong> {data?.email}
+        </p>
+        <p className="text-slate-900 mb-4">
+          <strong>Candidate Level:</strong> {data?.level}
+        </p>
+        <Link href="/" className="bg-blue-500 text-white p-2 rounded">
+          Go Back
+        </Link>
       </div>
     </div>
   );
